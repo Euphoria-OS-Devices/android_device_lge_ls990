6e05508df45b0d398f7d1f845ca27ba09e7b5957
777jon <jcruzdeveloper@gmail.com>
2015-03-07 07:33:15 -0500

    ls990: Euphorize
    Signed-off-by: 777jon <jcruzdeveloper@gmail.com>

--- eos.mk
+++ eos.mk
@@ -18,11 +18,24 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit from ls990 device
-$(call inherit-product, device/lge/ls990/device.mk)
+$(call inherit-product, device/lge/ls990/ls990.mk)
+
+# Inherit some common stuff.
+$(call inherit-product, vendor/eos/config/common_full_phone.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_DEVICE := ls990
-PRODUCT_NAME := full_ls990
+PRODUCT_NAME := eos_ls990
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := LG-ls990
 PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_DEVICE="g3" \
+    PRODUCT_NAME="g3_spr_us" \
+    BUILD_FINGERPRINT="lge/g3_spr_us/g3:4.4.2/KVT49L.LS990ZV4/LS990ZV4.1403169216:user/release-keys" \
+    PRIVATE_BUILD_DESC="g3_spr_us-user 4.4.2 KVT49L.LS990ZV4 LS990ZV4.1403169216 release-keys"
+
+# Copy device specific prebuilt files.
+PRODUCT_COPY_FILES += \
+    vendor/eos/prebuilt/common/bootanimations/BOOTANIMATION-1080x1920.zip:system/media/bootanimation.zip
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,1 @@
-add_lunch_combo cm_ls990-userdebug
+add_lunch_combo eos_ls990-userdebug
